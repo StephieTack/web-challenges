c33379b7830b8acfa8ab15b684e6b39fbbd4ee9e
Stephanie Tack <stephierathjen@gmx.de>
2024-06-12 13:02:43 +0200

    easier solution with changed html and css

--- js-basics_dark-mode/js/index.js
+++ js-basics_dark-mode/js/index.js
@@ -2,6 +2,18 @@ const bodyElement = document.querySelector('[data-js="body"]');
 
 const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
 darkModeButton.addEventListener("click", () => {
-  bodyElement.classList.add("body.dark");
+  bodyElement.classList.add("dark");
 });
 console.log(darkModeButton);
+
+const lightModeButton = document.querySelector('[data-js="light-mode-button"]');
+lightModeButton.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+});
+console.log(lightModeButton);
+
+const toggleModeButton = document.querySelector('[data-js="toggle-button"]');
+toggleModeButton.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});
+console.log(toggleModeButton);
