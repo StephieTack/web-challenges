9f99223bc84af9b26bc965bc7543fe64761f0b89
Stephanie Tack <stephierathjen@gmx.de>
2024-06-17 13:44:50 +0200

    solving challenge

---
+++ js-conditions-and-booleans_teenager-check/js/index.js
@@ -0,0 +1,23 @@
+const input = document.querySelector('[data-js="age-input"]');
+const button = document.querySelector('[data-js="age-button"]');
+const output = document.querySelector('[data-js="age-output"]');
+button.addEventListener("click", () => {
+  // Number(x) converts the string from the input to a number
+  // Beware: input values are *always* strings!
+  const age = Number(input.value);
+  // Exercise:
+  // Use conditions and the logical AND operator to write
+
+  // Variante 1
+  // "You are a teen." or "You are not a teen." into the output.
+  //   if (age >= 13 && age <= 18) {
+  //     output.textContent = "You are a teen.";
+  //   } else {
+  //     output.textContent = "You are not a teen.";
+  //   }
+  // });
+
+  // Variante 2
+  const ageMessage = age >= 13 && age <= 18 ? "ja issa." : "nein issa nicht";
+  output.textContent = ageMessage;
+});
