017c1c8802f8cd31ae20dd5c61dba661f85f30d9
Stephanie Tack <stephierathjen@gmx.de>
2024-07-18 13:07:28 +0200

    corrected solution

--- react-basics_attributes/src/App.js
+++ react-basics_attributes/src/App.js
@@ -1,14 +1,13 @@
-import React from "react";
 import "./styles.css";
 
 function ArticleComponent() {
   return (
     <article className="article">
       <h2 className="article__title">dies ist meine überschrift</h2>
-      <label htmlFor="input_id"></label>
-      <input id="input_id"></input>
-      <a className="article__link" href="https://www.wikipedia.de">
-        a tag
+      <label htmlFor="input_id">label</label>
+      <input placeholder="hier kommt text rein" id="input_id" />
+      <a href="https://www.wikipedia.de" className="article__link">
+        Link to Wikipedia
       </a>
     </article>
   );
