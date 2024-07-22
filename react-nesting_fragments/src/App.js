da411bc54f512ec0e2730631516b8777336ee457
Stephanie Tack <stephierathjen@gmx.de>
2024-07-22 14:51:47 +0200

    modified

--- react-nesting_fragments/src/App.js
+++ react-nesting_fragments/src/App.js
@@ -3,19 +3,15 @@ import "./styles.css";
 import Box from "./components/Box";
 
 export default function App() {
-  return (
-    <main className="flex-container">
-      <Boxes />
-    </main>
-  );
+  return <Boxes />;
 }
 
 function Boxes(color) {
   return (
-    <>
+    <main className="flex-container">
       <Box color="#007bff" />
       <Box color="#fc3" />
       <Box color="#ff3333" />
-    </>
+    </main>
   );
 }
