64f44c4582cc3b470aa5496f1855a39ef06d3830
Stephanie Tack <stephierathjen@gmx.de>
2024-07-22 15:29:34 +0200

    solved buttons and children

--- react-nesting_buttons-and-children/src/App.js
+++ react-nesting_buttons-and-children/src/App.js
@@ -4,18 +4,18 @@ import "./styles.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>button 1</Button>
+      <Button>b2</Button>
+      <Button>b3</Button>
+      <Button>b4</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
