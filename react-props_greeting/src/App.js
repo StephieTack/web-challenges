d27f58ef4b74bcddcf5fee225f84bddd5ff57067
Stephanie Tack <stephierathjen@gmx.de>
2024-07-22 14:59:12 +0200

    up to date

--- react-props_greeting/src/App.js
+++ react-props_greeting/src/App.js
@@ -1,23 +1,51 @@
 import "./styles.css";
 
-function Greeting({ name, isCoach }) {
-  let coach = null;
-  if (isCoach) {
-    coach = <span>hello coach</span>;
-  }
-  return (
-    { Coach }
-    {name}
-  ) 
-}
+// mein Ansatz
+// function Greeting({ name, isCoach }) {
+//   let coach = null;
+//   if (isCoach) {
+//     coach = <span>hello coach</span>;
+//   }
+//   return (
+//     { Coach }
+//     {name}
+//   )
+// }
 
+// export default function App() {
+//   return (
+//     <>
+//       <Greeting name="Stephie" />
+//       <Greeting name="Andrea" />
+//     </>
+//   );
+// }
+
+// // {isCoach ? "Hello" + }
+
+
+// Lösungsvariante 1:
+// export default function App() {
+//   return <Greeting name="Felix" />;
+// }
+
+// function Greeting({ name }) {
+//   const coaches = ["Andrea", "Felix", "Stephie"];
+//   return (
+//     <h1>{coaches.includes(name) ? "Hello, Coach!" : `Hello, ${name}!`}</h1>
+//   );
+// }
+
+// Lösungsvariante 2. Gefällt mir besser:
 export default function App() {
   return (
     <>
-      <Greeting name="Stephie" />
-      <Greeting name="Andrea" />
+      <Greeting name="Tobias" />
+      <Greeting name="Johanna" isCoach />
     </>
   );
 }
 
-// {isCoach ? "Hello" + }
+function Greeting({ name, isCoach }) {
+  return <h1>Hello, {isCoach ? "Coach" : name}!</h1>;
+}
